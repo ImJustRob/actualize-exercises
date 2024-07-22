295f9066f380e53859ab7b175117332fbcdd277a
ImJustRob <robertalothian@hotmail.com>
2024-07-22 11:30:33 -0400

    Create 111.rb

---
+++ 111.rb
@@ -0,0 +1,19 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+puts 2 + 2
+puts 5 * 6
+puts 72 / 6
+puts 1000 - 17
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+Puts 3 + 4 #111.rb:12:in `<main>': undefined method `Puts' for main:Object (NoMethodError) Did you mean?  putc
+#I typed in Puts with a capital P which needed to be lowercase because ruby is cap sensitive.
+puts 8 x 8 #111.rb:13: syntax error, unexpected tIDENTIFIER, expecting end-of-inputputs 8 x 8
+# i put the incorrect symbol for multiplication the correct symbol is *
+12 - 5 puts #no error message 
+#puts is in the incorrect place it needs to be in from of the equation.
+18 / 4 #no error message 
+# i didnt add puts to the equation so it didnt understand the line of code.
