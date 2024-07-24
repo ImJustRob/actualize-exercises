9b87908b757d89aa9486520fc45f85f1acd625e1
ImJustRob <robertalothian@hotmail.com>
2024-07-24 10:55:05 -0400

    Create 133.rb

---
+++ 133.rb
@@ -0,0 +1,27 @@
+# QUESTION 1
+# Write a program that uses a while loop to print the numbers from 1 to 20.
+# If the number is 7 or 15, it should also print out "Hey, that's my lucky number!"
+
+
+# QUESTION 2
+# Finish annotating the code below.
+# Then run the code to see if the output matches your annotation.
+number = 0             # 1
+while 2 > number       # 2 
+  if number < 1        # 
+    puts number        #  
+  end
+  number = number + 1  # 
+end
+puts "Done!"           # 
+
+# 1: number is 0
+# 2: (2 is greater than 0, continue)
+# 3: 
+# 4: 
+# 5: 
+# 6: 
+# 7: 
+# 8: 
+# 9: 
+# 10: 
