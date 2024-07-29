797ffdf83a8952a2ef064fbab402d19ae2861678
ImJustRob <robertalothian@hotmail.com>
2024-07-29 13:00:15 -0400

    Create 211.rb

---
+++ 211.rb
@@ -0,0 +1,14 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write a program that uses a variable to store a number, then prints the number times 10.
+x = 15 
+puts x * 10
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#i did figure it out but i did forget where to put the * symbol i made a line that said x = 15 * 10 then tried to do puts x but it gave me 15 so i shifted everything to 1 line and it made more sense.
+# i feel likei just need to take my time with problem solving i try to rush instead of taking my time.
