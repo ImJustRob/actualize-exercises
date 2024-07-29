5027e40d3e724caa8c4f544d0fb477262363ffd1
ImJustRob <robertalothian@hotmail.com>
2024-07-29 15:38:13 -0400

    add exercise solutions

---
+++ 212.rb
@@ -0,0 +1,17 @@
+# QUESTION 1
+# Write code to store a number using a variable, then a string using a different variable.
+# Then add the two variables together (by converting the number to a string) and 
+# store the result in a third variable. Finally, print out the third variable.
+# x = 8
+# y = "rob"
+# z = x, y
+# puts z
+
+# QUESTION 2
+# The code below has a mistake in it. Run the code to see the error message,
+# then explain what the error message means and what would need to change to fix it.
+x = 10
+y = x
+puts x + y
+#you printed on the wrong line you would have to swap line 14 and 15 because you have to establish what you veriables are prior to printing.
+
