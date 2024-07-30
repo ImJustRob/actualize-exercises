f8b234ada5d7955bcf6a50d18d6a5c516333626a
ImJustRob <robertalothian@hotmail.com>
2024-07-30 11:51:28 -0400

    222.rb

---
+++ 222.rb
@@ -0,0 +1,22 @@
+# QUESTION 1
+# Write a program that asks the user for their name.
+# If their name is Martin, print "Hi Martin!" to the screen.
+# Otherwise print "You're not Martin."
+puts "What is your name?"
+name = gets.chomp.downcase
+if name == "martin"
+    puts "hi martin"
+else
+    puts "youre not martin"
+end
+
+# QUESTION 2
+# Fix the indentation and the errors with the code below.
+x = 1000
+if x < 5
+    puts 0
+elsif x < 10
+    puts 1
+else
+    puts 2
+end
