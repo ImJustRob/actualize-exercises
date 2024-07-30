888f01c9169b3e40d1ec05c66cd701f9183a38de
ImJustRob <robertalothian@hotmail.com>
2024-07-30 11:37:27 -0400

    221.rb

---
+++ 221.rb
@@ -0,0 +1,14 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+x = "carl"
+y = "sam"
+puts x.upcase + y.upcase
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#Yes it was easy to solve, I would probably practice tomorrow how to add veriables into a longer sentance with spaces.
