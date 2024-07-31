ca9f26c6ff528191d0eeee972eb25689efefd4ae
ImJustRob <robertalothian@hotmail.com>
2024-07-31 12:25:16 -0400

    231.rb

---
+++ 231.rb
@@ -0,0 +1,12 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+x = "Robert"
+y = "Lothian"
+puts x + " " + y
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# yes i was able to easily solve this problem. i will try to practice string interpolation tomorrow.
+
