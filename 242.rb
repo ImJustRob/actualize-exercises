31faa01f5654982b68e7bc84f433171d20e8d869
ImJustRob <robertalothian@hotmail.com>
2024-08-01 10:20:02 -0400

    242.rb

---
+++ 242.rb
@@ -0,0 +1,12 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+colors = ["red", "green", "blue"]
+colors.push("yellow")
+pp colors
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+definition = {"equal" => "being the same", "actualize" => "make a reality of"}
+definition["measurement"] = "the action of measuring something."
+pp definition
