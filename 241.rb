18c87f28746776b0d7ac86bc2fc62f86e9efba2a
ImJustRob <robertalothian@hotmail.com>
2024-08-01 09:51:00 -0400

    241.rb

---
+++ 241.rb
@@ -0,0 +1,12 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+first = "robert"
+last = "lothian"
+puts " my name is #{first} #{last}"
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+#yes this was easy to solve and i will be practicing conditions
+
