5692050c50fb93c7137a289bfae00b4e07ab20e4
ImJustRob <robertalothian@hotmail.com>
2024-08-02 09:25:34 -0400

    251.rb

---
+++ 251.rb
@@ -0,0 +1,14 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write a program that asks the user to input a word. If the word is "marco", print "polo".
+puts "input a word"
+word = gets.chomp
+if word == "marco"
+    puts "polo"
+end
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# this one wasnt so easy and i had everything right except when i put equals i only put 1 instead of 2. I have to remember the small details.
+
