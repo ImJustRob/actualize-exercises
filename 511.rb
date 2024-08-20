9678cbcf8a7ea38f0b7ba4af7473a9d81f3e5659
ImJustRob <robertalothian@hotmail.com>
2024-08-19 22:56:00 -0400

    511, 512, 513, 514

---
+++ 511.rb
@@ -0,0 +1,15 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+x = 5
+if x < 0
+    puts 1776
+else
+    puts 1979
+end
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+#this one was pretty easy to remember ill try a different condition tomorrow.
+
