7cbd84e07fb3d3ffedcd5e8822b62f602f956099
ImJustRob <robertalothian@hotmail.com>
2024-08-20 19:57:59 -0400

    521, 522, 523, 524

---
+++ 521.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Use a variable to store a number, 
+# then write a condition that prints 100 if the number equals 100, 
+# prints 99 if the number is equal to 99, and prints 0 otherwise.
+number = 100
+if number == 100
+    puts 100
+elsif number == 99
+    puts 99
+else
+    puts 0
+end
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
